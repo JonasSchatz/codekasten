0378b562276f4f8eb8d62880955c6d3b44235693
Jonas Schatz <jonas.schatz@googlemail.com>
2020-12-11 15:19:46 +0100

    Backlinks in Sidebar

--- src/core/NoteGraph.ts
+++ src/core/NoteGraph.ts
@@ -1,11 +1,11 @@
-import { Graph } from 'graphlib';
+import { Edge, Graph } from 'graphlib';
 import { Uri } from 'vscode';
 import { Note } from './Note';
 import { Parser } from './Parser';
 import * as md5 from 'md5';
 
 import * as vscode from 'vscode'; //Todo: Remove
-import { MarkdownLink } from './Link';
+import { Link, MarkdownLink } from './Link';
 import { Event, Emitter } from './common/event';
 
 export class NoteGraph {
@@ -35,11 +35,6 @@ export class NoteGraph {
         const id: string = md5(note.path);
         var nodeAlreadyExists: boolean = this.graph.hasNode(id);
 
-        if(nodeAlreadyExists) {
-            this.graph.removeNode(id);
-            console.log(`setNote ${note.path}: Updating, remove original`);
-        }
-
         this.graph.setNode(id, note);
 
         for (const forwardLink of note.links) {
@@ -50,24 +45,21 @@ export class NoteGraph {
                     isStub: true, 
                     backlinks: [], 
                     title: ''
-                }
+                };
                 this.graph.setNode(md5(forwardLink.target), stub);
             }
             this.graph.setEdge(md5(forwardLink.source), md5(forwardLink.target));
         }
 
         if (nodeAlreadyExists) {
-            console.log(`setNote ${note.path}: Updating, set new`);
             this.onDidUpdateNoteEmitter.fire(id);
         } else {
-            console.log(`setNote ${note.path}: Added new node`);
             this.onDidAddNoteEmitter.fire(id);
         }
     }
 
     deleteNote(id: string) {
         this.graph.removeNode(id);
-        console.log(`deleteNote ${id}: Deledted`);
         this.onDidDeleteEmitter.fire(id);
     }
 
@@ -90,5 +82,15 @@ export class NoteGraph {
             }
         }
         return positiveNotes;
+    }
+
+    getBacklinks(targetId: string): Note[] {
+        const edges: void | Edge[] = this.graph.inEdges(targetId);
+
+        if(!edges){
+            return [];
+        } else {
+            return edges.map((edge) => this.graph.node(edge.v));
+        } 
     }
 }
---
+++ src/features/backlinkTreedataProvider.ts
@@ -0,0 +1,78 @@
+import * as vscode from 'vscode';
+import { Feature } from "./feature";
+import { NoteGraph } from "../core";
+import { openNoteInWorkspace } from "../vscode/NoteActions";
+import * as md5 from 'md5';
+import { Note } from '../core/Note';
+
+const feature: Feature = {
+    activate: (context: vscode.ExtensionContext, graph: NoteGraph) => {
+        const backlinkTreedataProvider: BacklinkTreedataProvider = new BacklinkTreedataProvider(graph);
+        vscode.window.registerTreeDataProvider('backlinks', backlinkTreedataProvider);
+
+        vscode.window.onDidChangeActiveTextEditor((editor) => {
+            backlinkTreedataProvider.refresh();
+        });
+
+        vscode.commands.registerCommand('backlinks.selectNode', (treeItem: TreeItem) => {
+            openNoteInWorkspace(treeItem.resourceUri.fsPath, vscode.ViewColumn.One);
+        });
+    }
+};
+
+
+class BacklinkTreedataProvider implements vscode.TreeDataProvider<TreeItem> {
+
+    graph: NoteGraph;
+
+    private _onDidChangeTreeData: vscode.EventEmitter<TreeItem | undefined> = new vscode.EventEmitter<TreeItem | undefined>();
+    readonly onDidChangeTreeData: vscode.Event<TreeItem | undefined> = this._onDidChangeTreeData.event;
+    refresh(): void {
+        this._onDidChangeTreeData.fire(undefined);
+    }
+
+    constructor(treeviewInputGraph: NoteGraph) {
+        this.graph = treeviewInputGraph;    
+    }
+
+    getChildren(element?: TreeItem): TreeItem[] {
+        const treeItems: TreeItem[] = [];
+
+        const currentlyOpen = vscode.window.activeTextEditor?.document?.uri.fsPath;
+        if(!currentlyOpen) {return treeItems;};
+
+        const notes: Note[]  = this.graph.getBacklinks(md5(currentlyOpen));
+        for(const note of notes){
+            const treeItem: TreeItem = {
+                label: note.title, 
+                children: undefined, 
+                resourceUri: vscode.Uri.file(note.path)
+            };
+            treeItem.command = {
+                command: "backlinks.selectNode", 
+                title: "Select Node", 
+                arguments: [treeItem]
+            };
+            treeItems.push(treeItem);
+        }
+        return treeItems;
+      }
+  
+    getTreeItem(element: TreeItem): vscode.TreeItem|Thenable<vscode.TreeItem> {
+      return element;
+    }
+}
+
+class TreeItem extends vscode.TreeItem {
+    children: TreeItem[]|undefined;
+
+    constructor(label: string, children?: TreeItem[]) {
+        super(
+            label,
+            children === undefined ? vscode.TreeItemCollapsibleState.None :
+                                    vscode.TreeItemCollapsibleState.Expanded);
+        this.children = children;
+    }
+}
+
+export default feature;
